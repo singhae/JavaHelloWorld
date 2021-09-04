df950e23a0f57a5584d885da1d0aa55fd8c921d8
ssinghae <thsgo1357@naver.com>
2021-09-04 17:58:42 +0900

    merge

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -1,8 +1,16 @@
 package com.example.lab1;
 
+import java.util.Scanner;
+
 public class Main {
-	public static void main() {
-		System.out.println("hello");
-		;;fskfodwnf
+	
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Hello World!!!\n");
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("[" + name + "]님 환영니다~");
+		
+		
 	}
 }
